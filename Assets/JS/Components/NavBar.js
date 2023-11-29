f6a1a658ff9dbf62485813ba4fbe8936a5171196
Waeel Mohammed <waeel.moh97@gmail.com>
2023-11-29 20:44:42 +0300

    The index page is done 29/11/2023

--- Assets/JS/Components/NavBar.js
+++ Assets/JS/Components/NavBar.js
@@ -1,8 +1,32 @@
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+var menuIcon = document.getElementById('menu-icon');
+    var sidebar = document.getElementById('sidebar');
+
+    menuIcon.addEventListener('click', function () {
+        if (sidebar.style.width === '300px') {
+            sidebar.style.width = '0';
+        } else {
+            sidebar.style.width = '300px';
+        }
+    });
+
+    document.addEventListener('click', function (event) {
+        var isClickInsideSidebar = sidebar.contains(event.target);
+        var isClickOnMenuIcon = (event.target === menuIcon);
+
+        if (!isClickInsideSidebar && !isClickOnMenuIcon) {
+            sidebar.style.width = '0';
+        }
+    });
+
+    document.addEventListener("DOMContentLoaded", function () {
+      const navbar = document.getElementById("scrolling");
+  
+      window.addEventListener("scroll", function () {
+          if (window.scrollY > 50) {
+              navbar.classList.add("scrolled");
+          } else {
+              navbar.classList.remove("scrolled");
+          }
+      });
+  });
+  
